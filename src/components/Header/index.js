c630ed86f70320e4c10a47edec0c34796ef568a1
Naveen-HN <hnnaveen93@gmail.com>
2020-09-10 15:14:18 -0400

    react router/ custom layouts / footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./styles.scss";
+
+import { Link } from "react-router-dom";
 import Logo from "./../../assets/logo.png";
 
 const Header = (props) => {
@@ -7,9 +9,18 @@ const Header = (props) => {
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="fotoShop Logo" />
+          <Link to="/">
+            <img src={Logo} alt="fotoShop Logo" />
+          </Link>
+        </div>{" "}
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">Register</Link>
+            </li>
+          </ul>
         </div>
-      </div>
+      </div>{" "}
     </header>
   );
 };
