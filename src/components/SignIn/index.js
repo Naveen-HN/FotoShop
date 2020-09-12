978c4f919102db79a454950f5090bf065e3e6ccb
Naveen-HN <hnnaveen93@gmail.com>
2020-09-12 18:18:15 -0400

    signIn with email and password done

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,29 +1,68 @@
 import React, { Component } from "react";
 import "./styles.scss";
 import Button from "./../forms/Button";
-import { signInWithGoogle } from "./../../firebase/utils";
+import { signInWithGoogle, auth } from "./../../firebase/utils";
+import FormInput from "./../forms/FormInput";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...initialState };
+  }
+
   handleSubmit = async (e) => {
     e.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ ...initialState });
+    } catch (err) {}
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
+    const { email, password } = this.state;
     return (
       <div className="signin">
         <div className="wrap">
           <h2> LogIn </h2>{" "}
           <div className="formWrap">
             <form onSubmit={this.handleSubmit}>
+              <FormInput
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                handleChange={this.handleChange}
+              />
+              <FormInput
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                handleChange={this.handleChange}
+              />
+              <Button type="submit">LogIn</Button>
               <div className="socialSignin">
                 <div className="row">
                   <Button onClick={signInWithGoogle}>
-                    Sign in with Google
-                  </Button>
-                </div>
-              </div>
-            </form>
-          </div>
+                    Sign in with Google{" "}
+                  </Button>{" "}
+                </div>{" "}
+              </div>{" "}
+            </form>{" "}
+          </div>{" "}
         </div>{" "}
       </div>
     );
